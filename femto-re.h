002d2e4e192dd374e73b35506c76ab2e933bc9a9
Koji Ota <afterjnih@gmail.com>
2022-01-02 02:53:50 +0900

    print A

---
+++ femto-re.h
@@ -0,0 +1,115 @@
+//
+// Created by koji on 1/1/22.
+//
+
+
+#ifndef FEMTO_RE_ALPHABETS_H
+#define FEMTO_RE_ALPHABETS_H
+
+using namespace std;
+
+class BMP {
+    byte file_type[2] = {byte{'B'}, byte{'M'}};
+    byte file_size[4] = { byte{0xfe}, byte{0x0a}, byte{0}, byte{0} };
+    byte file_header_reserved[4] = {byte{0}, byte{0}, byte{0}, byte{0}};
+    byte offset_to_date[4] = {byte{0x36}, byte{0}, byte{0}, byte{0}};
+
+    byte info_header_size[4] = {byte{0x28}, byte{0}, byte{0}, byte{0}};
+    byte height_in_pixels[4] = {byte{0x1e}, byte{0}, byte{0}, byte{0}};
+    byte width_in_pixels[4] = {byte{0x1e}, byte{0}, byte{0}, byte{0}};
+    byte number_of_color_planes[2] = {byte{1}, byte{0}};
+    byte number_of_bits_per_pixel[4] = {byte{0x18}, byte{0}};
+    byte compression_method[4] = {byte{0}, byte{0}, byte{0}, byte{0}};
+    byte image_date_size[4] = {byte{0xc8}, byte{0x0a}, byte{0}, byte{0}};
+    byte horizontal_resolution[4] = {byte{1}, byte{0}, byte{0}, byte{0}};
+    byte vertical_resolution[4] = {byte{1}, byte{0}, byte{0}, byte{0}};
+    byte number_of_colors_in_palette[4] = {byte{0}, byte{0}, byte{0}, byte{0}};
+    byte number_of_important_colors[4] = {byte{0}, byte{0}, byte{0}, byte{0}};
+
+public:
+    void generate(int bitmap[30][30]) {
+        std::ofstream tmp("tmp.bmp", std::ios::out | std::ios::binary | std::ios::trunc);
+        for (auto e : file_type)
+            tmp.put((unsigned char)e);
+        for (auto e : file_size)
+            tmp.put((unsigned char)e);
+        for (auto e : file_header_reserved)
+            tmp.put((unsigned char)e);
+        for (auto e : offset_to_date)
+            tmp.put((unsigned char)e);
+        for (auto e : info_header_size)
+            tmp.put((unsigned char)e);
+        for (auto e : height_in_pixels)
+            tmp.put((unsigned char)e);
+        for (auto e : width_in_pixels)
+            tmp.put((unsigned char)e);
+        for (auto e : number_of_color_planes)
+            tmp.put((unsigned char)e);
+        for (auto e : number_of_bits_per_pixel)
+            tmp.put((unsigned char)e);
+        for (auto e : compression_method)
+            tmp.put((unsigned char)e);
+        for (auto e : image_date_size)
+            tmp.put((unsigned char)e);
+        for (auto e : horizontal_resolution)
+            tmp.put((unsigned char)e);
+        for (auto e : vertical_resolution)
+            tmp.put((unsigned char)e);
+        for (auto e : number_of_colors_in_palette)
+            tmp.put((unsigned char)e);
+        for (auto e : number_of_important_colors)
+            tmp.put((unsigned char)e);
+
+        for (int i = 0; i < 30; i++) {
+            auto y = bitmap[29 - i];
+            for (int j = 0; j < 30; j++)
+                if (y[j] == 0) {
+                    tmp.put((unsigned)255);
+                    tmp.put((unsigned)255);
+                    tmp.put((unsigned)255);
+                } else {
+                    tmp.put((unsigned)0);
+                    tmp.put((unsigned)0);
+                    tmp.put((unsigned)0);
+                }
+            tmp.put((unsigned)0);
+            tmp.put((unsigned)0);
+        }
+    }
+};
+
+
+int BitMapA[30][30] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0}
+};
+
+#endif //FEMTO_RE_ALPHABETS_H
